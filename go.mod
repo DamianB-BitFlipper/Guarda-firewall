d5605db4850366bd5b32df7ca96f528f6e6b29c5
Damian Barabonkov <damianb@mit.edu>
2021-01-21 19:38:46 -0500

    Working reverse proxy acting as intermediary for RESTful front-end and back-end

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/webauthn-firewall
+
+go 1.14
+
+require unknwon.dev/clog/v2 v2.2.0
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	log "unknwon.dev/clog/v2"
+)
+
+type WebauthnFirewall struct {
+	*httputil.ReverseProxy
+}
+
+func logRequest(r *http.Request) {
+	log.Info("%s:\t%s", r.Method, r.URL)
+}
+
+func (proxy *WebauthnFirewall) handleRequest(verbose bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if verbose {
+			logRequest(r)
+		}
+
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	reverseProxyPort := 8081
+	origin, _ := url.Parse("http://localhost:8080/")
+
+	director := func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		req.URL.Scheme = "http"
+		req.URL.Host = origin.Host
+	}
+
+	// Initialize a webauthn firewall
+	firewall := &WebauthnFirewall{
+		&httputil.ReverseProxy{Director: director},
+	}
+
+	http.HandleFunc("/api/", firewall.handleRequest(true))
+
+	log.Info("Starting up server on port: %d", reverseProxyPort)
+	log.Fatal("%v", http.ListenAndServe(fmt.Sprintf(":%d", reverseProxyPort), nil))
+
+	// Graceful stopping all loggers before exiting the program.
+	log.Stop()
+}
+
+func init() {
+	err := log.NewConsole()
+	if err != nil {
+		panic("unable to create new logger: " + err.Error())
+	}
+}
