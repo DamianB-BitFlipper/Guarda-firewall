7821a0b6f7bb347ff42dee08c1540dba33afe181
Damian Barabonkov <damianb@mit.edu>
2021-01-27 11:30:30 -0500

    Wrote custom logger wrapper for database logging

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -28,10 +29,88 @@ func Init() error {
 	return nil
 }
 
+// Create a logger interface for the `log` library
+type dbLogger struct {
+	logLevel      logger.LogLevel
+	slowThreshold time.Duration
+}
+
+var _ logger.Interface = (*dbLogger)(nil)
+
+var (
+	defaultLogger = &dbLogger{
+		logLevel:      logger.Warn,
+		slowThreshold: 200 * time.Millisecond,
+	}
+)
+
+func (l *dbLogger) LogMode(level logger.LogLevel) logger.Interface {
+	newLogger := *l
+	newLogger.logLevel = level
+	return &newLogger
+}
+
+func (l dbLogger) Info(_ context.Context, msg string, data ...interface{}) {
+	if l.logLevel >= logger.Info {
+		//log.Info(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
+		log.Info(msg, data...)
+	}
+}
+
+func (l dbLogger) Warn(_ context.Context, msg string, data ...interface{}) {
+	if l.logLevel >= logger.Warn {
+		//log.Info(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
+		log.Warn(msg, data...)
+	}
+}
+
+func (l dbLogger) Error(_ context.Context, msg string, data ...interface{}) {
+	if l.logLevel >= logger.Error {
+		//log.Info(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
+		log.Error(msg, data...)
+	}
+}
+
+// Trace print sql message
+func (l dbLogger) Trace(_ context.Context, begin time.Time, fc func() (string, int64), err error) {
+	var (
+		traceStr     = "[%.3fms] [rows:%v] %s"
+		traceWarnStr = "%s [%.3fms] [rows:%v] %s"
+		traceErrStr  = "%s [%.3fms] [rows:%v] %s"
+	)
+
+	if l.logLevel > logger.Silent {
+		elapsed := time.Since(begin)
+		switch {
+		case err != nil && l.logLevel >= logger.Error:
+			sql, rows := fc()
+			if rows == -1 {
+				log.Error(traceErrStr, err.Error(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
+			} else {
+				log.Error(traceErrStr, err.Error(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+			}
+		case elapsed > l.slowThreshold && l.slowThreshold != 0 && l.logLevel >= logger.Warn:
+			sql, rows := fc()
+			slowLog := fmt.Sprintf("SLOW SQL >= %v", l.slowThreshold)
+			if rows == -1 {
+				log.Warn(traceWarnStr, slowLog, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+			} else {
+				log.Warn(traceWarnStr, slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+			}
+		case l.logLevel == logger.Info:
+			sql, rows := fc()
+			if rows == -1 {
+				log.Info(traceStr, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+			} else {
+				log.Info(traceStr, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+			}
+		}
+	}
+}
+
 func newDB() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("webauthn-firewall.db"), &gorm.Config{
-		// TODO: User `log` module instead of gorm logger
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: defaultLogger,
 		NowFunc: func() time.Time {
 			return time.Now().Local()
 		},
